75d0805a50d8ea7d115a8483eb22e9b3f5817efd
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2013-10-12 17:07:10 +0000

    use npm from nodejs, not npm cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@simplymeasured.com'
 license          'All rights reserved'
 description      'Installs/Configures chef-scylla'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends 'git'
 depends 'imagemagick'
