34842dbd55805b3547994cb9b6a1dda1a07f2606
sdierauf <sdierauf@gmail.com>
2014-08-21 11:48:17 -0700

    got rid of config file creation as those will be made post deploy with shim

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,32 +23,6 @@ directory "#{node['scylla']['prefix']}/scylla/images/resources" do
   action :create
 end
 
-directory "#{node['scylla']['prefix']}/scylla/config" do
-  owner 'root'
-  group 'root'
-  mode '775'
-  recursive true
-  action :create
-end
-
-template "#{node['scylla']['prefix']}/scylla/config/storage.js" do
-  owner 'root'
-  group 'root'
-  mode '664'
-  variables(
-    scylla: node['scylla']
-  )
-end
-
-template "#{node['scylla']['prefix']}/scylla/config/database.js" do
-  owner 'root'
-  group 'root'
-  mode '664'
-  variables(
-    scylla: node['scylla']
-  )
-end
-
 service 'scylla' do
   provider Chef::Provider::Service::Upstart
   supports :status => true, :restart => true, :reload => true
