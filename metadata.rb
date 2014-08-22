669c379533ac3f45e79d054c4f316928517ba19b
sdierauf <sdierauf@gmail.com>
2014-08-22 16:07:04 -0700

    changed dashes to underscores

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@simplymeasured.com'
 license          'All rights reserved'
 description      'Installs/Configures chef-scylla'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.10'
+version          '1.0.11'
 
 depends 'imagemagick'
 depends 'nodejs'
